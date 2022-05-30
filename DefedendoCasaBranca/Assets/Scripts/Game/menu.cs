0a158421ad20bf9177ae5cdf8e1985385f45868e
RLMurta <rafaellmurta@hotmail.com>
2022-05-30 16:58:47 -0300

    colocando o menu

---
+++ DefedendoCasaBranca/Assets/Scripts/Game/menu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    /**
+     * Muda scene de acordo com string passada
+     * scene -> nome da cena a ser ativada
+     */
+    public void mudaScene(string scene) {
+        SceneManager.LoadScene(scene);
+    }
+
+    /**
+     * Muda scene ativa de volta para homepage (Menu) 
+     */
+    public void mudaSceneHome()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
